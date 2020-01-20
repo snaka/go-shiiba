da8278280eb797c321c88ee870bbfbcfe82ff22e
snaka <snaka.gml@gmail.com>
2020-01-21 08:38:56 +0900

    add test

--- calendar.go
+++ calendar.go
@@ -4,11 +4,13 @@ import (
   "time"
 )
 
+// Activity stores activity count at date
 type Activity struct {
   Date time.Time
   Count int
 }
 
+// Activities stores collection of activities during a specified period
 type Activities struct {
   data   []*Activity
   From  time.Time
@@ -16,18 +18,16 @@ type Activities struct {
 }
 
 // NewActivities initalize and return instance
-func NewActivities() *Activities {
-  now := time.Now()
+func NewActivities(now time.Time) *Activities {
   from := now.AddDate(0, 0, -364)
-  a := Activities{
+  return &Activities{
     data: make([]*Activity, 365),
     From: from,
     To: now,
   }
-  return &a
 }
 
-//
+// Iterate activities
 func (p *Activities) Iterate(cb func(act *Activity) error) error {
   for i := 0; i < 10; i++ {
     err := cb(p.data[i])
--- calendar_test.go
+++ calendar_test.go
@@ -2,16 +2,22 @@ package shiiba
 
 import (
   "testing"
+  "time"
   "reflect"
 )
 
 func TestNewActivities(t *testing.T) {
-  a := NewActivities()
+  now, _ := time.Parse(time.RFC3339, "2020-01-01T00:00:00Z00:00")
+  a := NewActivities(now)
   if a == nil {
     t.Errorf("NewActivities is nil")
   }
   expectedTypeName := "*shiiba.Activities"
   if reflect.TypeOf(a).String() != expectedTypeName {
      t.Errorf("fail: want %s but %T", expectedTypeName, a)
+  }
+  expectedFromDate, _ := time.Parse(time.RFC3339, "2019-01-01T00:00:00Z00:00")
+  if !expectedFromDate.Equal(a.From) {
+    t.Errorf("fail: want %v but %v", expectedFromDate, a.From)
   }
 }
