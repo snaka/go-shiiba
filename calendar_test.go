4d6b048de7023a548956d34f2a6ce17c2d1440fb
snaka <snaka.gml@gmail.com>
2020-01-23 05:35:41 +0900

    refactoring test code

--- calendar_test.go
+++ calendar_test.go
@@ -10,6 +10,15 @@ var (
   now = time.Date(2020, 1, 1, 0, 0, 0, 0, time.UTC)
 )
 
+func createSamples(count int) []time.Time {
+  expects := make([]time.Time, count)
+  for i := 0; i < count; i++ {
+    offset := count - i - 1
+    expects[i] = now.Add(time.Duration(24 * -offset) * time.Hour)
+  }
+  return expects
+}
+
 func TestNewActivities(t *testing.T) {
   a := NewActivities(now, 10)
   if a == nil {
@@ -26,17 +35,10 @@ func TestNewActivities(t *testing.T) {
 }
 
 func TestIterate(t *testing.T) {
-  a := NewActivities(now, 3)
-  expectFirst := now
-  expects := []time.Time {
-    expectFirst.Add(time.Duration(24 * -2) * time.Hour),
-    expectFirst.Add(time.Duration(24 * -1) * time.Hour),
-    expectFirst,
-  }
+  expects := createSamples(10)
+  a := NewActivities(now, len(expects))
   a.Iterate(func(i int, act Activity) error {
-    expected := expects[i]
-    actual := act.Date
-    if expected.Equal(actual) == false {
+    if !expects[i].Equal(act.Date) {
       t.Errorf("#%d: date not expected: got: %v want: %v", i, act.Date, expects[i])
     }
     return nil
