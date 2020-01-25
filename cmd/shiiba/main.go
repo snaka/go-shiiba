2858edf13aa53f87392e6843c6508b967e222928
snaka <snaka.gml@gmail.com>
2020-01-25 18:43:05 +0900

    parse commandline options

--- cmd/shiiba/main.go
+++ cmd/shiiba/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+  "flag"
   "log"
   "os"
   "time"
@@ -8,14 +9,20 @@ import (
   shiiba "github.com/snaka/go-shiiba"
 )
 
+var (
+  showWeekday *bool = flag.Bool("w", false, "Show weekday")
+  showMonth *bool = flag.Bool("m", false, "Show month")
+)
+
 func main() {
-  err := shiiba.ShowCalendar(
-    os.Stdout,
-    time.Now(),
-    365,
-    shiiba.IsShowWeekday(true),
-    shiiba.IsShowMonth(true),
-  )
+  flag.Parse()
+
+  opts := []shiiba.Option {
+    shiiba.IsShowWeekday(*showWeekday),
+    shiiba.IsShowMonth(*showMonth),
+  }
+
+  err := shiiba.ShowCalendar(os.Stdout, time.Now(), 365, opts...)
   if err != nil {
     log.Fatal(err)
   }
